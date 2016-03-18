49f41f7215c126ff057a97cf0d33145be4025814
nori <bgnori@gmail.com>
2016-03-18 19:27:55 +0900

    fix deck tests

--- lib/deck_test.go
+++ lib/deck_test.go
@@ -2,30 +2,53 @@ package npoker
 
 import (
 	"fmt"
-	//	"testing"
+	"testing"
 )
 
-func SmokeDeck() {
-	d := Deck{Card{ACE, SPADES}, Card{FOUR, HEARTS}}
-	fmt.Println(d)
-	e := BuildFullDeck()
-	fmt.Println(e)
-	fmt.Println(e.Length())
-	e.Shuffle()
-	fmt.Println(e)
-	e.Shuffle()
-	fmt.Println(e)
-	f := BuildFullDeck()
-	for i, c := range []Card(*f) {
-		fmt.Println(i, c, c.Ord())
+func deckhelper(t *testing.T, expected string, to_string Deck) {
+	if expected != fmt.Sprintf("%v", to_string) {
+		t.Errorf("'%s' is expected for %d, but got %v", expected, to_string, to_string)
 	}
-	f.Drop(Card{ACE, HEARTS})
-	fmt.Println(f.Length())
-	fmt.Println(f)
-	f.Shuffle()
-	fmt.Println(f)
+}
 
-	g := Deck{Card{ACE, SPADES}, Card{ACE, CLUBS}}
-	h := Deck{Card{KING, HEARTS}, Card{KING, DIAMONDS}}
-	fmt.Println(Join(g, h))
+func TestDeckString(t *testing.T) {
+	d := Deck{Card{ACE, SPADES}, Card{FOUR, HEARTS}}
+	deckhelper(t, "A\u2660,4\u2665", d)
+}
+
+func TestFullDeck(t *testing.T) {
+	d := BuildFullDeck()
+	if d.Length() != 52 {
+		t.Errorf("full length is expected to be 52.")
+	}
+}
+
+func TestDropDeck(t *testing.T) {
+	d := Deck{Card{ACE, SPADES}, Card{ACE, CLUBS}}
+	d.Drop(Card{ACE, SPADES})
+	if d.Length() != 1 {
+		t.Errorf("expected is 1.")
+	}
+	deckhelper(t, "A\u2663", d)
+}
+
+func TestJoinDeck(t *testing.T) {
+	d := Deck{Card{ACE, SPADES}, Card{ACE, CLUBS}}
+	e := Deck{Card{KING, HEARTS}, Card{KING, DIAMONDS}}
+	f := Join(d, e)
+	if f.Length() != 4 {
+		t.Errorf("expected is 4.")
+	}
+	deckhelper(t, "A\u2660,A\u2663,K\u2665,K\u2666", f)
+}
+
+func TestShuffle(t *testing.T) {
+	d := BuildFullDeck()
+	if d.Length() != 52 {
+		t.Errorf("full length is expected to be 52.")
+	}
+	d.Shuffle()
+	if d.Length() != 52 {
+		t.Errorf("full length is expected to be 52.")
+	}
 }
