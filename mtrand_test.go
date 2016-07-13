fc2a06d30234792f0998a1aa5b074d7f0ed8cc15
nori <bgnori@gmail.com>
2016-07-13 15:31:19 +0900

    have benchmarks

--- mtrand_test.go
+++ mtrand_test.go
@@ -12,3 +12,18 @@ func TestLoadSeed(t *testing.T) {
 	fmt.Println(r.Int63())
 
 }
+
+func BenchmarkGetSeedFromURAND(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		GetSeedFromURAND()
+	}
+}
+
+func BenchmarkIntn(b *testing.B) {
+	r := NewRand()
+	r.SeedFromBytes(GetSeedFromURAND())
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		r.Int63()
+	}
+}
