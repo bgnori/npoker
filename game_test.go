dee86c8510fbd6737edff68593834191e34fd63c
bgnori <bgnori@gmail.com>
2021-06-14 16:39:06 +0900

    more tests

--- game_test.go
+++ game_test.go
@@ -73,7 +73,6 @@ func TestPSReaderfeedLine_STARTOFHAND(t *testing.T) {
 }
 
 
-
 func TestPSReaderfeedLine_SEATPLAYER(t *testing.T) {
 	sample := `Seat 1: adevlupec (53368 in chips)`
 	reader := NewPSReader()
@@ -85,4 +84,175 @@ func TestPSReaderfeedLine_SEATPLAYER(t *testing.T) {
 	}
 }
 
+func TestPSReaderfeedLine_SETBTN(t *testing.T) {
+	sample := `Seat 1: adevlupec (53368 in chips)`
+	reader := NewPSReader()
+	mock := NewMock()
+	reader.line = mock
+	reader.feedLine(sample)
+	if mock.foo != SETBTN{
+		t.Errorf("%v is expected but got %v",  SETBTN, mock.foo)
+	}
+}
 
+
+func TestPSReaderfeedLine_POSTSB(t *testing.T) {
+	sample := `FluffyStutt: posts small blind 50`
+	reader := NewPSReader()
+	mock := NewMock()
+	reader.line = mock
+	reader.feedLine(sample)
+	if mock.foo != POSTSB{
+		t.Errorf("%v is expected but got %v",  POSTSB, mock.foo)
+	}
+}
+
+
+func TestPSReaderfeedLine_POSTBB(t *testing.T) {
+	sample := `adevlupec: posts big blind 100`
+	reader := NewPSReader()
+	mock := NewMock()
+	reader.line = mock
+	reader.feedLine(sample)
+	if mock.foo != POSTBB{
+		t.Errorf("%v is expected but got %v",  POSTBB, mock.foo)
+	}
+}
+
+
+func TestPSReaderfeedLine_DEALX(t *testing.T) {
+	sample := `Dealt to FluffyStutt [2h Ks]`
+	reader := NewPSReader()
+	mock := NewMock()
+	reader.line = mock
+	reader.feedLine(sample)
+	if mock.foo != DEALX{
+		t.Errorf("%v is expected but got %v",  DEALX, mock.foo)
+	}
+}
+
+func TestPSReaderfeedLine_FOLD(t *testing.T) {
+	sample := `FluffyStutt: folds`
+	reader := NewPSReader()
+	mock := NewMock()
+	reader.line = mock
+	reader.feedLine(sample)
+	if mock.foo != FOLD{
+		t.Errorf("%v is expected but got %v",  FOLD, mock.foo)
+	}
+}
+
+
+func TestPSReaderfeedLine_BET(t *testing.T) {
+	//UGH! no testing data
+	sample := `FluffyStutt: folds`
+	reader := NewPSReader()
+	mock := NewMock()
+	reader.line = mock
+	reader.feedLine(sample)
+	if mock.foo != BET{
+		t.Errorf("%v is expected but got %v",  BET, mock.foo)
+	}
+}
+
+
+func TestPSReaderfeedLine_RAISE(t *testing.T) {
+	//UGH! no testing data
+	sample := `FluffyStutt: folds`
+	reader := NewPSReader()
+	mock := NewMock()
+	reader.line = mock
+	reader.feedLine(sample)
+	if mock.foo != RAISE{
+		t.Errorf("%v is expected but got %v",  RAISE, mock.foo)
+	}
+}
+
+func TestPSReaderfeedLine_CHECK(t *testing.T) {
+	sample := `adevlupec: checks`
+	reader := NewPSReader()
+	mock := NewMock()
+	reader.line = mock
+	reader.feedLine(sample)
+	if mock.foo != CHECK{
+		t.Errorf("%v is expected but got %v",  CHECK, mock.foo)
+	}
+}
+
+func TestPSReaderfeedLine_CALL(t *testing.T) {
+	sample := `Drug08: calls 100`
+	reader := NewPSReader()
+	mock := NewMock()
+	reader.line = mock
+	reader.feedLine(sample)
+	if mock.foo != CALL{
+		t.Errorf("%v is expected but got %v",  CALL, mock.foo)
+	}
+}
+
+func TestPSReaderfeedLine_PREFLOP(t *testing.T) {
+	sample := `*** HOLE CARDS ***`
+	reader := NewPSReader()
+	mock := NewMock()
+	reader.line = mock
+	reader.feedLine(sample)
+	if mock.foo != PREFLOP{
+		t.Errorf("%v is expected but got %v",  PREFLOP, mock.foo)
+	}
+}
+
+func TestPSReaderfeedLine_FLOP(t *testing.T) {
+	sample := `*** FLOP *** [8h 7s 8d]`
+	reader := NewPSReader()
+	mock := NewMock()
+	reader.line = mock
+	reader.feedLine(sample)
+	if mock.foo != FLOP{
+		t.Errorf("%v is expected but got %v",  FLOP, mock.foo)
+	}
+}
+
+
+func TestPSReaderfeedLine_TURN(t *testing.T) {
+	sample := `*** TURN *** [8h 7s 8d] [Th]`
+	reader := NewPSReader()
+	mock := NewMock()
+	reader.line = mock
+	reader.feedLine(sample)
+	if mock.foo != TURN{
+		t.Errorf("%v is expected but got %v",  TURN, mock.foo)
+	}
+}
+
+func TestPSReaderfeedLine_RIVER(t *testing.T) {
+	sample := `*** RIVER *** [8h 7s 8d Th] [2c]`
+	reader := NewPSReader()
+	mock := NewMock()
+	reader.line = mock
+	reader.feedLine(sample)
+	if mock.foo != RIVER{
+		t.Errorf("%v is expected but got %v",  RIVER, mock.foo)
+	}
+}
+
+func TestPSReaderfeedLine_SHOWDOWN(t *testing.T) {
+	sample := `*** SHOW DOWN ***`
+	reader := NewPSReader()
+	mock := NewMock()
+	reader.line = mock
+	reader.feedLine(sample)
+	if mock.foo != SHOWDOWN{
+		t.Errorf("%v is expected but got %v",  SHOWDOWN, mock.foo)
+	}
+}
+
+func TestPSReaderfeedLine_ENDOFHAND(t *testing.T) {
+	sample := `*** SUMMARY ***`
+	reader := NewPSReader()
+	mock := NewMock()
+	reader.line = mock
+	reader.feedLine(sample)
+	if mock.foo != ENDOFHAND{
+		t.Errorf("%v is expected but got %v",  ENDOFHAND, mock.foo)
+	}
+}
